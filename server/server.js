1c663e1b5c3f234294780d078bf4cbd9d3c41de9
Muchnik Illia <muchnic.illia.js@gmail.com>
2025-01-31 17:55:17 +0200

    Config cors

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const fileRoutes = require("./routes/fileRoutes");
+
 const app = express();
 
-app.use(cors());
-app.use(express.json()); 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
